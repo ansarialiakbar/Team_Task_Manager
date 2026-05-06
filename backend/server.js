aad50a61f09c2e3213215850febd417b102601df
ansarialiakbar <ansarialiakbar2001@gmail.com>
2026-05-06 14:03:40 +0530

    updated backend cors

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,8 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  // origin: "http://localhost:3000",
+  origin: process.env.FRONTEND_URL,
   credentials: true
 }));
 app.use(express.json());
